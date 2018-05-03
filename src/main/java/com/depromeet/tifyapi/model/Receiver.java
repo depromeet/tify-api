3ebbdfe4f6359cbae205f1f56faf3a4e8e2143bc
Haeseong Jeon <junhaesung0428@hanmail.net>
2018-05-03 21:52:20 +0900

    Add receiver model, mapper

--- src/main/java/com/depromeet/tifyapi/model/Receiver.java
+++ src/main/java/com/depromeet/tifyapi/model/Receiver.java
@@ -1,14 +1,20 @@
 package com.depromeet.tifyapi.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Receiver {
     private Integer receiverId;
     private String name;
-    private LocalDateTime anniversary;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date anniversary;
 }
